317c460400541f75e07fd5a201301d6f3ff4cafb
arpitapayal2005-wq <arpita.payal2005@gmail.com>
2026-06-08 19:50:21 +0530

    Refactor has_duplicate_brute function formatting

--- optimization_practice/contains_duplicate.py
+++ optimization_practice/contains_duplicate.py
@@ -65,8 +65,8 @@ Optimized:   False  # O(n)
 """
 def has_duplicate_brute(nums):
     for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] == nums[j]:
+        for j in range(i+1, len(nums)):
+            if nums[i]==nums[j]:
                 return True
     return False
 
@@ -78,7 +78,7 @@ def has_duplicate_fast(nums):
         seen.add(num)
     return False
 
-nums = [1, 2, 3, 1]
+nums=[1,2,3,1]
 
 print("Brute Force:", has_duplicate_brute(nums))
 print("Optimized:", has_duplicate_fast(nums))
